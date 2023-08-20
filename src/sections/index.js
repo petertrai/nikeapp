29c7ffbbccfd86eea708ff651ab11baa2933bc04
petertrai <petertrai@gmail.com>
2023-08-19 18:03:26 -0700

    initial

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero"
+import PopularProducts from "./PopularProducts"
+import Footer from "./Footer"
+import Services from "./Services"
+import SpecialOffer from "./SpecialOffer"
+import Subscribe from "./Subscribe"
+import SuperQuality from "./SuperQuality"
+import CustomerReviews from "./CustomerReviews"
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    Footer,
+    Subscribe,
+    CustomerReviews
+}
